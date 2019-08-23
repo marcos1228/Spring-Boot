71ec1b40a9623a4d819d66414467de521f1e91ff
marcos1228 <evangelistamarcos99@gmail.com>
2019-08-23 16:08:32 -0300

    camada de Domain

--- demo-mvc/src/main/java/com/mballem/curso/boot/DemoMvcApplication.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/DemoMvcApplication.java
@@ -5,15 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoMvcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMvcApplication.class, args);
-		
-		
-		
-		
-		
-		
-	}
 
+	}
 }
---
+++ demo-mvc/src/main/java/com/mballem/curso/boot/domain/Funcionario.java
@@ -0,0 +1,87 @@
+package com.mballem.curso.boot.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+
+@Table(name = "FUNCIONARIO")
+public class Funcionario extends AbstractEntity<Long> {
+
+	@Column(nullable = false, unique = true)
+	private String nome;
+
+	@Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
+	private BigDecimal salario;
+
+	@Column(name = "data_entrada", nullable = false, columnDefinition = "DATE")
+	private LocalDate dataEntrda;
+
+	@Column(name = "data_saida", nullable = true, columnDefinition = "DATE")
+	private LocalDate datasaida;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "endereco_id_fk")
+	private Endereco endereco;
+
+	@ManyToOne
+	@JoinColumn(name = "cargo_id_fk")
+	private Cargo cargo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	public LocalDate getDataEntrda() {
+		return dataEntrda;
+	}
+
+	public void setDataEntrda(LocalDate dataEntrda) {
+		this.dataEntrda = dataEntrda;
+	}
+
+	public LocalDate getDatasaida() {
+		return datasaida;
+	}
+
+	public void setDatasaida(LocalDate datasaida) {
+		this.datasaida = datasaida;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Cargo getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+}
